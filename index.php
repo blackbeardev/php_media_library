f5f6ed811722fbd7b602eb8e7e81e26611678a27
flyingfoxweb <erin@flyingfox.net.nz>
2017-05-26 22:09:09 +0000

    includes folder created with header.php and footer.php. New page called suggest.php created.

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
+<?php include ("includes/header.php"); ?>
 
-<html>
-<head>
-	<title><?php echo "Personal Media Library"; ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-<body>
 
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
-
-			<ul class="nav">
-                <li class="books"><a href="catalog.php?cat=books">Books</a></li>
-                <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
-                <li class="music"><a href="catalog.php?cat=music">Music</a></li>
-                <li class="suggest"><a href="suggest.php">Suggest</a></li>
-            </ul>
-
-		</div>
-
-	</div>
-
-	<div id="content">
 		<div class="section catalog random">
 
 			<div class="wrapper">
@@ -38,22 +15,4 @@
 
 		</div>
 
-	</div>
-
-	<div class="footer">
-
-		<div class="wrapper">
-
-			<ul>		
-				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
-				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
-			</ul>
-
-			<p>&copy; <?php echo date("Y"); ?> Personal Media Library</p>
-
-		</div>
-	
-	</div>
-
-</body>
-</html>
+<?php include("includes/footer.php"); ?>
---
+++ suggest.php
@@ -0,0 +1,8 @@
+<?php include ("includes/header.php"); ?>
+
+<div class="section page">
+    <h1>Suggest a Media Item</h1>
+</div>
+
+
+<?php include("includes/footer.php"); ?>
