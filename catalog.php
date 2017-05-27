e7e4ea1ea95dfa51c89ecf1eb0a2083f9c96b024
flyingfoxweb <erin@flyingfox.net.nz>
2017-05-27 05:07:04 +0000

    Add an array to display some items on the catalog page.

--- catalog.php
+++ catalog.php
@@ -1,4 +1,11 @@
 <?php 
+$catalog = array();
+//Tells php to add one new element to the code, using whatever index number comes next.
+$catalog[101] = "Design Patterns"; 
+$catalog[201] = "Forrest Gump";
+$catalog[301] = "Beethoven";
+$catalog[102] = "Clean Code";
+
 $pageTitle = "Full Catalog";
 
 //Set the active menu choice
@@ -26,11 +33,20 @@ include("includes/header.php");
 
 ?>
 
-<div class="section page">
-    <h1><?php echo $pageTitle; ?></h1>
+<div class="section catalog page">
+    <div class="wrapper">
+        <h1><?php echo $pageTitle; ?></h1>
+        
+            <ul>
+                <?php
+                    foreach($catalog as $item) {
+                        echo "<li>" . $item . "</li>";
+                    }
+                ?>
+            </ul>
+        
+    </div>
 </div>
-
-
 
 
 
