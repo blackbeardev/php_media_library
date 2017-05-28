407d49a4a17dc7c428a5d709acb045bb23ff676e
flyingfoxweb <erin@flyingfox.net.nz>
2017-05-28 22:14:15 +0000

    Sorting functionality added to each catalog pages and the full catalog page.

--- catalog.php
+++ catalog.php
@@ -32,7 +32,11 @@ include("includes/header.php");
 
 <div class="section catalog page">
     <div class="wrapper">
-        <h1><?php echo $pageTitle; ?></h1>
+        <h1><?php
+            if($section != null) {
+                echo "<a href='catalog.php'>Full Catalog</a> &gt;";
+            }
+        echo $pageTitle; ?></h1>
         
             <ul class="items">
                 <?php
--- includes/functions.php
+++ includes/functions.php
@@ -12,19 +12,22 @@ function get_item_html($id, $item) {
 
 function array_category($catalog, $category) {
     
-    if($category == null) {
-        return array_keys($catalog);
-    }
-    
     $output = array();
     
     foreach($catalog as $id => $item) {
-        if(strtolower($category) == strtolower($item["category"])) {
-            $output[] = $id;
+        if($category == null OR strtolower($category) == strtolower($item["category"])) {
+            $sort = $item["title"];
+            //Trim the, an or a from the titles starting from the left hand side of the name
+            $sort = ltrim($sort, "The ");
+            $sort = ltrim($sort, "A ");
+            $sort = ltrim($sort, "An ");
+            $output[$id] = $sort;
         } 
     }
     
-    return $output;
+    asort($output);
+    
+    return array_keys($output);
 }
 
 ?>
