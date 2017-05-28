83d6ff2e1887e39d4d62b9786f5ec012863d7d52
flyingfoxweb <erin@flyingfox.net.nz>
2017-05-28 21:30:22 +0000

    Added functionality for a random 4 items to be displayed on the index.php page instead of all items.  These 4 items will change each time the page is refreshed.

--- index.php
+++ index.php
@@ -17,8 +17,10 @@ include ("includes/header.php");
 
 				<ul class="items">
 					<?php
-                    	foreach($catalog as $id => $item) {
-                        	echo get_item_html($id, $item);
+					$random = array_rand($catalog, 4);
+					
+                    	foreach($random as $id) {
+                        	echo get_item_html($id, $catalog[$id]);
                     	}
                 	?>								
 				</ul>
