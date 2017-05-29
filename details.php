28b06e6c789b149a12e78052e4e2aa0eafce6cdc
flyingfoxweb <erin@flyingfox.net.nz>
2017-05-29 04:52:35 +0000

    Details added for individual items.

---
+++ details.php
@@ -0,0 +1,93 @@
+<?php 
+
+include("includes/data.php");
+include("includes/functions.php");
+
+
+
+//Check to see which category has been clicked on and set the page title to be the cat.
+if(isset($_GET["id"])) {
+    $id = $_GET["id"];
+    if(isset($catalog[$id])) {
+        $item = $catalog[$id];
+    }
+}
+
+if(!isset($item)) {
+    header("location:catalog.php");
+    exit;
+}
+
+$pageTitle = $item["title"];
+
+//Set the active menu choice
+$section = null;
+
+include("includes/header.php"); 
+
+?>
+
+<div class="section page">
+    <div class="wrapper">
+        <div class="media-picture">
+            <span><img src="<?php echo $item['img']; ?>" alt="<?php echo $item['title']; ?>" /></span>
+        </div>
+        
+        <div class="media-details">
+            <h1><?php echo $item["title"] ?></h1>
+            <table>
+                <tr>
+                    <th>Category</th>
+                    <td><?php echo $item["category"]; ?></td>
+                </tr>
+                <tr>
+                    <th>Genre</th>
+                    <td><?php echo $item["genre"]; ?></td>
+                </tr>
+                <tr>
+                    <th>Format</th>
+                    <td><?php echo $item["format"]; ?></td>
+                </tr>
+                <tr>
+                    <th>Year</th>
+                    <td><?php echo $item["year"]; ?></td>
+                </tr>
+                <?php
+                    if(strtolower($item["category"]) == "books") { ?>
+                        <tr>
+                            <th>Authors</th>
+                            <td><?php echo implode(", ",$item["authors"]); ?></td>
+                        </tr>
+                        <tr>
+                            <th>Publisher</th>
+                            <td><?php echo $item["publisher"]; ?></td>
+                        </tr>
+                        <tr>
+                            <th>ISBN</th>
+                            <td><?php echo $item["isbn"]; ?></td>
+                        </tr>
+                    <?php } else if(strtolower($item["category"]) == "movies") { ?> 
+                    <tr>
+                        <th>Director</th>
+                        <td><?php echo $item["director"]; ?></td>
+                    </tr>
+                    <tr>
+                       <th>Writers</th>
+                       <td><?php echo implode(", ",$item["writers"]); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Stars</th>
+                        <td><?php echo implode(", ",$item["stars"]); ?></td>
+                    </tr>
+                    <?php } else if(strtolower($item["category"]) == "music") { ?> 
+                    <tr>
+                        <th>Artist</th>
+                        <td><?php echo $item["artist"]; ?></td>
+                    </tr>
+                    <?php } ?>
+                
+            </table>
+        </div>
+        
+    </div>
+</div>
